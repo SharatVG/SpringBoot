f11166e4c1de613a7b9af009fcc3c3d2f3150da0
sgangal2 <sharat.gangal@dxc.com>
2022-01-18 14:10:07 +0530

    Initial Commit

---
+++ demo.user.role/demo.user.role/src/test/java/com/cs/demo/user/role/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.demo.user.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
